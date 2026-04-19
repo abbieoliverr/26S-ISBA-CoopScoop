6dad01092bfe85c3ba6e4410be3e24af3d5546fd
moshirzadehsara <moshirzadeh.s@northeastern.edu>
2026-04-19 10:36:15 -0400

    Fixed my own typos

--- app/src/pages/50_Student_Home.py
+++ app/src/pages/50_Student_Home.py
@@ -9,10 +9,10 @@ st.title(f"Welcome, {st.session_state['first_name']}!")
 st.write('### What would you like to do today?')
 
 if st.button('Browse Company Reviews & Interview Questions', use_container_width=True):
-    st.switch_page('pages/51_Student_Reviews.py')
+    st.switch_page('pages/52_Student_Rev.py')
 
 if st.button('My Applications & Notes', use_container_width=True):
-    st.switch_page('pages/52_Student_Applications.py')
+    st.switch_page('pages/51_student_apps.py')
 
 if st.button('Browse Co-op Listings', use_container_width=True):
-    st.switch_page('pages/53_Student_Listings.py') 
+    st.switch_page('pages/53_student_listing.py')
